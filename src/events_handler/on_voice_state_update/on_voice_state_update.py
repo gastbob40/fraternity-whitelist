eedadb4468402a7d4a8fb8367b6eea27ecddd7cb
gastbob40 <gastbob40@gmail.com>
2021-01-21 21:22:13 +0100

    feat (voice): add join detection

--- src/events_handler/on_voice_state_update/on_voice_state_update.py
+++ src/events_handler/on_voice_state_update/on_voice_state_update.py
@@ -1,8 +1,24 @@
 import discord
+import yaml
+
+# Get configuration
+with open("run/config.yml", 'r') as stream:
+    data = yaml.safe_load(stream)
 
 
 class OnVoiceStateUpdate:
 
     @staticmethod
     async def handle(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
-        pass
+        if member.bot:
+            return
+
+        # Case 1, the player join the channel
+        if after.channel and after.channel.id == data['waiting_channel']:
+            if before.channel is None or before.channel.id != data['waiting_channel']:
+                print(after.channel)
+
+        # Case 2, the player leaves the channel
+        if before.channel and before.channel.id == data['waiting_channel']:
+            if after.channel is None or after.channel.id != data['waiting_channel']:
+                print(before.channel)
